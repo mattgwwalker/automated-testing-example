fb44c37f96cd7deb578bde9aa60169621b8de940
Matthew Walker <mattgwwalker@gmail.com>
2019-05-23 10:05:50 -0400

    Initial version

---
+++ add.py
@@ -0,0 +1,8 @@
+def addTwoNumbers(x, y):
+    return 0
+
+
+if __name__ == "__main__":
+    print("Calling addTwoNumbers(1, 2)...")
+    result = addTwoNumbers(1, 2)
+    print("... result is "+str(result))
---
+++ test_add.py
@@ -0,0 +1,7 @@
+from add import addTwoNumbers
+
+def test_add_two_zeros():
+    assert(addTwoNumbers(0,0) == 0)
+
+#def test_add_one_and_one():
+#    assert(addTwoNumbers(1,1) == 2)
